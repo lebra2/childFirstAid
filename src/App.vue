b165482fa8ffae67fb9beb871583afc899fdb994
lebra2 <markus.timofejev@ametikool.ee>
2023-09-20 15:14:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="mt-12">
+    <h1 class="text-center text-4xl">childFirstAid</h1>
+  </div>
+</template>
