0a3941c6dd70f975f49a2ff3ab74d297ba5ef77d
lebra2 <markus.timofejev@ametikool.ee>
2023-09-21 10:09:52 +0300

    Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+        '4xl': [
+            '0 35px 35px rgba(0, 0, 0, 0.25)',
+            '0 45px 65px rgba(0, 0, 0, 0.15)'
+        ]
+      }
+    },
   },
   plugins: [],
 }
