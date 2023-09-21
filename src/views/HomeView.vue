526c562db10ff2d110d8e205689296efce76caaf
Maarma <Maarma25@gmail.com>
2023-09-21 08:57:33 +0300

    footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-
+import Footer from '../components/Footer.vue'
+import Header from '../components/Header.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Header />
+    <Footer />
+
   </main>
 </template>
