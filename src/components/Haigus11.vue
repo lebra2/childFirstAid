ef927b5182c1ea9069ae63f17f4977b9c74d057f
lebra2 <markus.timofejev@ametikool.ee>
2023-09-21 16:52:26 +0300

    defineProps info muutmiseks

--- src/components/Haigus11.vue
+++ src/components/Haigus11.vue
@@ -1,11 +1,27 @@
+<script setup>
+let visible = false;
+
+const props = defineProps({
+    videoLink: String,
+    sellestPara: String,
+    haiguseEnnet1: String,
+    haiguseEnnet2: String,
+    haiguseEnnet3: String,
+    teeSeda1: String,
+    teeSeda2: String,
+    teeSeda3: String,
+    teeSeda4: String,
+})
+</script>
+
 <template>
     <div class="bg-[#F6F6F7] text-xl font-[Sans-Serif]">
         <div class="">
-            <iframe class="p-4 w-full h-64" src="https://www.youtube.com/embed/GymXjJJ7Ugo?si=cz_xKLKxEahJ3Xf0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe class="p-4 w-full h-64" :src="videoLink" allowfullscreen></iframe>
         </div>
         <div class="p-4 text-sm">
             <h1 class="text-lg">Sellest:</h1>
-        <p class="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem alias mollitia animi corporis ex optio natus sit, commodi aut consequatur veniam, explicabo, totam sapiente. Distinctio tenetur molestias explicabo ut quam!</p>
+        <p class="">{{ sellestPara }}</p>
         </div>
         
         <ul>
@@ -14,82 +30,29 @@
         <ol class="text-sm space-y-1 list-decimal list-inside p-4">
             <h1 class="text-lg">Haiguse ennetus:</h1>
 
-            <li>You might feel like you are being really "organized" o</li>
-            <li>Nested navigation in UIs is a bad idea too, keep things as flat as possible.</li>
-            <li>Nesting tons of folders in your source code is also not helpful.</li>
+            <li>{{ haiguseEnnet1 }}</li>
+            <li>{{ haiguseEnnet2 }}</li>
+            <li>{{ haiguseEnnet3 }}</li>
         </ol>
             </li>
         </ul>
         <div class=text-md>
             <div class="px-4">
-            <RouterLink to="/haigus1" class="bg-white h-14 rounded-b-md font-medium border-b-2 items-center justify-between flex"><p class="m-2">Tee seda</p></RouterLink>
+            <RouterLink to="/haigus1" class="bg-white h-14 rounded-b-md font-medium border-b-2 items-center justify-between flex"><p class="m-2">{{ teeSeda1 }}</p></RouterLink>
             </div>
             <div class="px-4">
-            <RouterLink to="/haigus1" class="bg-white h-14 rounded-b-md font-medium border-b-2 items-center justify-between flex"><p class="m-2">Tee seda</p></RouterLink>
+            <RouterLink to="/haigus1" class="bg-white h-14 rounded-b-md font-medium border-b-2 items-center justify-between flex"><p class="m-2">{{ teeSeda2 }}</p></RouterLink>
             </div>
             <div class="px-4">
-            <RouterLink to="/haigus1" class="bg-white h-14 rounded-b-md font-medium border-b-2 items-center justify-between flex"><p class="m-2">Tee seda</p></RouterLink>
+            <RouterLink to="/haigus1" class="bg-white h-14 rounded-b-md font-medium border-b-2 items-center justify-between flex"><p class="m-2">{{ teeSeda3 }}</p></RouterLink>
             </div>
             <div class="px-4">
-            <RouterLink to="/haigus1" class="bg-white h-14 rounded-b-md font-medium border-b-2 items-center justify-between flex"><p class="m-2">Tee seda</p></RouterLink>
+            <RouterLink to="/haigus1" class="bg-white h-14 rounded-b-md font-medium border-b-2 items-center justify-between flex"><p class="m-2">{{ teeSeda4 }}</p></RouterLink>
             </div>
         </div>
         <div class="p-2">
             <h1 class="font-bold text-xl ml-2 my-4">Q&As</h1>
-        <container>
-            <div class="drop-shadow-md bg-white mt-3 p-2">
-                <div class="">
-                <button class="" @click="visible = !visible"><p class="">Mida teha kui mul jalg sygeleb 🡻</p></button>
-            </div>
-        
-            <div class="" v-if="visible">
-                <div class="">
-                    <p class=text-sm>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, maxime dolor a, iusto ad rerum velit accusamus quidem libero!</p>
-                </div>
-            </div>
-            </div>
-
-        </container>
-        <container>
-            <div class="drop-shadow-md bg-white mt-3 p-2">
-                <div class="">
-                <button class="" @click="visible = !visible"><p class="">Mida teha kui mul jalg sygeleb 🡻</p></button>
-            </div>
-        
-            <div class="" v-if="visible">
-                <div class="">
-                    <p class=text-sm>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, maxime dolor a, iusto ad rerum velit accusamus quidem libero!</p>
-                </div>
-            </div>
-            </div>
-
-        </container>
-        <container>
-            <div class="drop-shadow-md bg-white mt-3 p-2">
-                <div class="">
-                <button class="" @click="visible = !visible"><p class="">Mida teha kui mul jalg sygeleb 🡻</p></button>
-            </div>
-        
-            <div class="" v-if="visible">
-                <div class="">
-                    <p class=text-sm>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, maxime dolor a, iusto ad rerum velit accusamus quidem libero!</p>
-                </div>
-            </div>
-            </div>
-
-        </container>
         </div>
 
     </div>
 </template>
-
-<script>
-  export default {
-    data() {
-      return {
-        visible: false,
-      };    
-    },
-  };
-
-</script>
--- src/views/Ennetamine1.vue
+++ src/views/Ennetamine1.vue
@@ -10,7 +10,17 @@ import Haigus11 from '../components/Haigus11.vue';
   <main>
     <Header />
     <h1>Olukordade Ennetamine 1</h1>
-    <Haigus11/>
+    <Haigus11 video-link="https://www.youtube.com/watch?v=vclQ6gLFOOw"
+    sellest-para="sadfasdfasf"
+    haiguse-ennet1="asdfasdfasfd"
+    haiguse-ennet2="asdfasdf"
+    haiguse-ennet3="asdfasdf"
+    tee-seda1="asdfasdf"
+    tee-seda2="asdasd"
+    tee-seda3="asdasdasd"
+    tee-seda4="asdasdasd"/>
+    <Footer />
+
     <Footer />
 
   </main>
