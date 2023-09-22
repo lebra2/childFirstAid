68b22666d19ac6473972eb326a65b38b328a96ca
aliinS <aliin.sepp@gmail.com>
2023-09-22 10:30:27 +0300

    mumps done, tuulerouged done, lakakoha done

--- src/views/TuuleRouged.vue
+++ src/views/TuuleRouged.vue
@@ -9,25 +9,19 @@ import Haigus11 from '../components/Haigus11.vue';
 <template>
   <main>
     <Header />
-    <h1>TuuleRouged</h1>
+    <h1>Tuulerõuged</h1>
     <Haigus11 video-link="https://www.youtube.com/watch?v=vclQ6gLFOOw"
-    sellest-para="sadfasdfasf"
-    haiguse-ennet1="asdfasdfasfd"
-    haiguse-ennet2="asdfasdf"
-    haiguse-ennet3="asdfasdf"
-    tee-seda1="asdfasdf"
-    tee-seda2="asdasd"
-    tee-seda3="asdasdasd"
-    tee-seda4="asdasdasd"
-    midateha-v=""
-    midateha-v2=""
-    midateha-v3=""
-    kysimus1=""
-    kysimus2=""
-    kysimus3=""
-    vastus1=""
-    vastus2=""
-    vastus3=""/>
+    sellest-para="Tuulerõuged ehk varitsella-zoster viiruse põhjustatud nakkus on üsna levinud lapsepõlvehaigus, mis võib esineda ka täiskasvanutel. Tuulerõuged avalduvad tavaliselt palaviku, sügeluse ja nahal esinevate väikeste punaste laikude või villidega. Need villid võivad sügeleda väga tugevalt. Haigus võib põhjustada ka üldist halba enesetunnet."
+    haiguse-ennet1="Vaktsineerimine."
+    midateha-v="Tuulerõugete puhul puudub spetsiifiline viirusevastane ravi. Enamik inimesi paraneb iseenesest mõne nädala jooksul. Sümptomite leevendamiseks võib kasutada sügelusevastaseid kreeme, antihistamiine ja palavikualandajaid."
+    midateha-v2="Nakatunud inimesed peaksid jääma kodusele isolatsioonile, et vältida haiguse levikut teistele, eriti kuni viimaste uute villide kuivamiseni."
+    midateha-v3="On olemas vaktsiin, mis kaitseb tuulerõugete vastu. MMRV-vaktsiin (leetrid, mumps, punetised, tuulerõuged) on sageli kasutusel laste vaktsineerimisprogrammides. See vaktsiin võib aidata vähendada haiguse levimust ja tõsiste komplikatsioonide teket."
+    kysimus1="Kas tuulerõuged võivad põhjustada komplikatsioone?"
+    kysimus2="Lasteaias käis eile laps, kellel olid tuulerõuged. Millal tean, kas mu laps jäi haigeks?"
+    kysimus3="Mis põhjustab tuulerõugeid?"
+    vastus1="Kuigi tuulerõuged on enamikul juhtudel kerge haigus, võivad nad teatud juhtudel põhjustada tõsiseid komplikatsioone, eriti täiskasvanutel, rasedatel ja immuunpuudulikkusega inimestel. Komplikatsioonide hulka võivad kuuluda kopsupõletik, aju põletik ja nahainfektsioonid."
+    vastus2="Pärast nakatumist kulub tavaliselt 10 kuni 21 päeva, enne kui esimesed sümptomid ilmnevad."
+    vastus3="Tuulerõugeid põhjustab varitsella-zoster viirus (VZV), mis kuulub herpesviiruste perekonda. Sama viirus põhjustab ka vöötohatisi (herpes zoster) juhul, kui see aktiveerub uuesti organismis hiljem elus."/>
     <Footer />
 
     <Footer />
