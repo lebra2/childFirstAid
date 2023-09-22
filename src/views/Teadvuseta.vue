9b513c4bf372ebeb16b972cf3e8b0310f4a33ae4
Kala <kkallas640@gmail.com>
2023-09-22 11:32:03 +0300

    teadvuseta laps

--- src/views/Teadvuseta.vue
+++ src/views/Teadvuseta.vue
@@ -11,23 +11,34 @@ import Haigus11 from '../components/Haigus11.vue';
     <Header />
     <h1>Teadvuseta</h1>
     <Haigus11 video-link="https://www.youtube.com/watch?v=vclQ6gLFOOw"
-    sellest-para="sadfasdfasf"
-    haiguse-ennet1="asdfasdfasfd"
-    haiguse-ennet2="asdfasdf"
-    haiguse-ennet3="asdfasdf"
-    tee-seda1="asdfasdf"
-    tee-seda2="asdasd"
-    tee-seda3="asdasdasd"
-    tee-seda4="asdasdasd"
-    midateha-v=""
-    midateha-v2=""
-    midateha-v3=""
-    kysimus1=""
-    kysimus2=""
-    kysimus3=""
-    vastus1=""
-    vastus2=""
-    vastus3=""/>
+    sellest-para="Tähtis on südame funktsiooni kiire normaliseerimine ja peaaju hapnikuga varutatuse taastamine ning säilitamine.
+    Esimestel minutitel teostatud esmased elustamisvõtted on suurendanud ellujäämist kaks kuni kolm korda.
+    "
+    haiguse-ennet1="Kui laps on teadvusetu, kuid hingab ja süda lõõb"
+    tee-seda1="Põõrake laps külili"
+    tee-seda2="Tõstke tema pealmine jalg kõrverdatult nii, et põlv toetaks keha asendit"
+    tee-seda3="Pealmine käsi tõstke kõverdatult ette ja sättige labakäsi käeseljaga põse alla"
+    haiguse-ennet2="Kui laps on teadvusetu ega hinga, kuid süda lõõb"
+    tee-seda4="Asetage laps selili kõvale alusele ja kallutage ühe käega tema pea ettevaatlikult kuklasse ja tõstke teise käega lapse lõuga"
+    tee-seda5="Avage lapse suu ning alustage suust suhu hingamist, kattes imikul oma suuga ka ninasõõrmed, suuremal lapsel ainult suu, samal ajal nina näppudega kinni hoides"
+    tee-seda6="Hingake sisse ja puhuge õhk lapse suhu(jälgige, et lapse rindkere tõuseks, see on märgiks, et õhk jõub kopsudesse)"
+    tee-seda7="Nüüd vabastage suu ja laske õhul lapse kopsudest välja voolata"
+    haiguse-ennet3="Kui laps on teadvusetu, ei hinga ja tema süda ei lõõ"
+    tee-seda8="Alustage hingamisteede vabastamisest"
+    tee-seda9="Südamemassaažiks pange oma labakäsi lapse rindkerele rinnaku alusele poolele ja suruge nii, et rindkere vajuks umber 1/3 võrra sisse"
+    tee-seda10="Imiku südamemassaažiks vajutage rinnakule kahe sõrmega"
+    haiguse-ennet4="Elustamiskatse korral tuleb elustamist jätkata seni, kuni on kindlaks määratud järgmised asjaolud:"
+    midateha-v="On taastatud efektiivne vereringe ja hingamine"
+    midateha-v2="Patsient on üle antud kiirabibrigaadile"
+    midateha-v3="Abiandja (päästja) ei jõua enam väsimuse tõttu tegevust jätkata; ümbritsev keskkond ohustab abiandjate elu"
+    kysimus1="Mitu korda tuleb südamemassaži ajal kompressioone teha?"
+    kysimus2="Kust kontrollitakse pulssi alla 1 aastasel lapsel?"
+    kysimus3="Mida tähendab CAB"
+    vastus1="100 korda minutis ehk umber 2 korda sekundis"
+    vastus2="Õlavarre siseküljelt"
+    vastus3="C(circulation) - kaudne südamemassaaž
+    A(airway) - hingamisteede avamine
+    B(breathing) - kunstlik hingamine"/>
     <Footer />
 
     <Footer />
