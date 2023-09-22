26c904439ab436a1ba15b793c558f558ba9cbd9f
henrikmutt <henrik.mutt@gmail.com>
2023-09-22 11:30:10 +0300

    mürgistused KKK

--- src/views/Myrgistused2.vue
+++ src/views/Myrgistused2.vue
@@ -22,12 +22,12 @@ import Haigus11 from '../components/Haigus11.vue';
     midateha-v=""
     midateha-v2=""
     midateha-v3=""
-    kysimus1=""
-    kysimus2=""
-    kysimus3=""
-    vastus1=""
-    vastus2=""
-    vastus3=""/>
+    kysimus1="Milliseid esmaabivahendeid võiks koju varuda? Kas üldse on sellised olemas?"
+    kysimus2="Kas on mingi rusikareegel käitumiseks, kui laps on mõnda kodukeemiavahendit joonud või tabletti söönud?"
+    kysimus3="Oskate näiteid tuua, kust lapsed on mürgiseid asju kätte saanud? Või millised iga päev kasutatavad asjad võiks igaks juhuks laste silme alt ära panna?"
+    vastus1="Igas kodus peaks olemas olema aktiivsüsi. See ei maksa palju ning õigete säilitamistingimuste korral praktiliselt aegumatu, ent võib üllatavalt paljude mürgituste korral olla elupäästvaks vahendiks. Igas kodus võiks tablettide arv, mida varuda, võrduda lapse neljakordse kehakaaluga. Näiteks 10kg lapse puhul 40 tabletti. See on kogus, mida võib vaja minna ja mis on efektiivne. Enne sellise koguse aktiivsõe manustamist tuleb esmalt konsulteerida mürgistusinfoliiniga (16662)"
+    vastus2="Jah ikka on. Eelkõige suu loputada ja veenduda, et seal ei ole tableti/kemikaalijääke. Seejärel anda väikeste koguste kaupa juua vett, mis lahjendaks allaneelatud mürki. Kindlasti pole mõtet ajada last oksendama, eriti söövitavate ainete puhul on see lausa keelatud. Teistpidi toimuks oksendamise korral uus söögitoru söövitus halvendades seeläbi kannatanu seisundit veelgi."
+    vastus3="Alati peaksid olema lastega peredes kõrgele ära pandud olema alkohol, nikotiinitooted, söövitavad ained, käekotid (kuna sisaldavad pea alati mõnda ravimit vm lastele mürgist ainet)."/>
     <Footer />
 
     <Footer />
